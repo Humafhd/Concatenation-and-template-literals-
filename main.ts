13440243755349f3a57e77736ba0368e1441a58c
Humafhd <fhuma302@gmail.com>
2024-03-11 16:26:24 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+//concatenation
+console.log('hello',' world');//using comma to print two strings
+console.log('hello' + 'this is huma');//this + is acocatenate type,prints without space
+console.log('hello'+(' ')+'this is huma');//this + is acocatenate type,prints with space 
+console.log(2+2);//this is operation
+let num1=30
+let num2=54
+let sum =num1+num2
+console.log('the sum of num1 and num2 will be :');//string type not concatenate
+console.log('the sum of ' + num1 +(' ') + 'and'+(' ') + num2 + 'will be' + sum);//+(' ')+ this with space gvs space
+console.log('the sum of ' + num1 + 'and' + num2 + 'will be\n'+sum);//+(' ')+ this with space gvs space,& \n to print outout in new line 
+// cocatenate can read \n but not line spacing like template literalls
+
+//template literalls
+console.log(`the sum of ${num1} and ${num2} is ${sum}`);//use bcktic for call.variable
+console.log(`the sum of ${num1} and ${num2} is
+ ${sum}`);//use bcktic for call.variable also see space if given b/w the string .also prints in new line.but in cocatenation not possible
+console.log(`the sum of ${num1} and ${num2} is \n ${sum}`);//use bcktic for call.variable,also can use \n to print in new line
+
+
+
+
+
+
